818383afc80e1359f9baa1b0559fb962e0bb6cd1
pratyush2105 <pratyush.civ21@iitg.ac.in>
2024-02-10 00:27:49 +0530

    Add chat page, utils, components

---
+++ client/src/utils/APIRoutes.js
@@ -0,0 +1,4 @@
+export const host = 'http://localhost:3001';
+export const allUsersRoute = `${host}/profile/allchats`;
+export const sendMessageRoute = `${host}/messages/addmsg`;
+export const getAllMessageRoute = `${host}/messages/getmsg`;
